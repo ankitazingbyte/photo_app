578fba15f4bd669a23dc0a31cc0bf65deaa2f6b1
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-27 13:57:14 +0530

    overall commit

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+Rails.configuration.stripe = {
+  :publishable_key => ENV['pk_test_RE7y1XcDVS3iDCt4BLIMM6x4'],
+  :secret_key      => ENV['sk_test_f0kppmcboNfj77D8s0vYTYEO
+']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
